a4e4ce85cf8fb425665de7153f430177edcc6450
Tanish Garge <=>
2025-06-10 18:48:35 +0530

    feat: started to receive all incoming transactions.

---
+++ src/main/java/com/jpmc/midascore/component/KafkaConsumer.java
@@ -0,0 +1,22 @@
+package com.jpmc.midascore.component;
+
+import com.jpmc.midascore.foundation.Transaction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaConsumer {
+
+    private static final Logger logger = LoggerFactory.getLogger(KafkaConsumer.class);
+
+    @KafkaListener(topics = "${general.kafka-topic}", groupId = "midas-core-group")
+    public void handleTransaction(Transaction transaction) {
+        logger.info("=== RECEIVED TRANSACTION ===");
+        logger.info("Transaction: {}", transaction);
+        logger.info("Amount: {}", transaction.getAmount());
+        logger.info("===========================");
+
+    }
+}
